3a65f01a0a369fb0afee22b3a4e78b2312ce9787
joaquinns <joaquindevv96@gmail.com>
2022-03-03 00:23:00 -0400

    setup project linter and prettier

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Navbar from './components/Navbar'
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <Navbar />
+      </header>
+
+      <main>
+        <p>This works!</p>
+      </main>
+    </div>
+  )
+}
+
+export default App
