c49d3873d6771ed832c6071cee02e7c4c368dfcd
joaquinns <joaquindevv96@gmail.com>
2022-03-05 17:35:20 -0400

    refactor to feed videos

--- src/components/MainContent/index.jsx
+++ src/components/MainContent/index.jsx
@@ -1,9 +1,5 @@
 import styles from './mainContentContainer.module.css'
 
 export default function MainContentContainer({ children }) {
-  return (
-    <div className={styles.main_content}>
-      <div className={styles.container}>{children}</div>
-    </div>
-  )
+  return <div className={styles.main_content}>{children}</div>
 }
