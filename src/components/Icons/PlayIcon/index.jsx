168a76468ec8b83afec393655fc370867e69a61c
joaquinns <joaquindevv96@gmail.com>
2022-03-04 23:59:14 -0400

    added icons and main styles

---
+++ src/components/Icons/PlayIcon/index.jsx
@@ -0,0 +1,13 @@
+export default function PlayIcon() {
+  return (
+    <svg
+      viewBox='0 0 48 48'
+      xmlns='http://www.w3.org/2000/svg'
+      width='20'
+      height='20'
+      fill='#000'
+    >
+      <use xlinkHref='#svg-play-fill'></use>
+    </svg>
+  )
+}
