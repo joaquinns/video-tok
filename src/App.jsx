7da5979cdd6ba6756c9ea0d4ee49c8c2a21e207d
joaquinns <joaquindevv96@gmail.com>
2022-03-06 16:09:26 -0400

    added auth context to app

--- src/App.jsx
+++ src/App.jsx
@@ -4,27 +4,30 @@ import SideNavbar from './components/SideNavbar'
 import MainContentContainer from './components/MainContent'
 import Home from './pages/Home'
 import { Route } from 'wouter'
+import { AuthContextProvider } from './context/AuthContext'
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <Navbar />
-      </header>
+    <AuthContextProvider>
+      <div className='App'>
+        <header className='App-header'>
+          <Navbar />
+        </header>
 
-      <main>
-        <SideNavbar />
-        <MainContentContainer>
-          <Route path='/'>
-            <Home />
-          </Route>
+        <main>
+          <SideNavbar />
+          <MainContentContainer>
+            <Route path='/'>
+              <Home />
+            </Route>
 
-          <Route path='/login'>
-            <h1>Login :D</h1>
-          </Route>
-        </MainContentContainer>
-      </main>
-    </div>
+            <Route path='/login'>
+              <h1>Login :D</h1>
+            </Route>
+          </MainContentContainer>
+        </main>
+      </div>
+    </AuthContextProvider>
   )
 }
 
