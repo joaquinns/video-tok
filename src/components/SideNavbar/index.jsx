84d29ac771f6983e8f6c708fe6b075de2148c600
joaquinns <joaquindevv96@gmail.com>
2022-03-09 13:31:50 -0400

    refactor sidebar component

--- src/components/SideNavbar/index.jsx
+++ src/components/SideNavbar/index.jsx
@@ -1,6 +1,7 @@
 import styles from './sidenavbar.module.css'
 import { HomeIcon } from '../Icons/HomeIcon'
 import { VideoIcon } from '../Icons/VideoIcon'
+import { Link } from 'wouter'
 
 // recordar hacer iconos
 
@@ -11,10 +12,12 @@ export default function SideNavbar() {
         <div className={styles.scroll_styled}>
           <div className={styles.wrapper}>
             <div className={styles.main_nav_actions}>
-              <a className={styles.nav_link}>
-                <HomeIcon />
-                <h2>Home</h2>
-              </a>
+              <Link to='/'>
+                <a className={styles.nav_link}>
+                  <HomeIcon />
+                  <h2>Home</h2>
+                </a>
+              </Link>
               <a className={styles.nav_link}>
                 <VideoIcon />
                 <h2>Video</h2>
