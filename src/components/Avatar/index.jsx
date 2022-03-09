2d667313f9bde1e27888ac592e30d9fcbe337db7
joaquinns <joaquindevv96@gmail.com>
2022-03-09 13:29:10 -0400

    refactor avatar component

--- src/components/Avatar/index.jsx
+++ src/components/Avatar/index.jsx
@@ -1,13 +1,17 @@
 import PropTypes from 'prop-types'
 import styles from './avatar.module.css'
 
-function Avatar({ width = 60, height = 60 }) {
+export default function Avatar({
+  srcImage = 'https://p16-sign-va.tiktokcdn.com/tos-maliva-avt-0068/12762135a3b5593013a8e94aabc4015f~c5_100x100.jpeg?x-expires=1646604000&x-signature=ZtQ%2Fb5IggYTohM2sYhG%2BUNjZEJo%3D',
+  width = 60,
+  height = 60
+}) {
   return (
     <div className={styles.avatar_container}>
       <img
         className={styles.avatar}
         loading='lazy'
-        src='https://p16-sign-va.tiktokcdn.com/tos-maliva-avt-0068/12762135a3b5593013a8e94aabc4015f~c5_100x100.jpeg?x-expires=1646604000&x-signature=ZtQ%2Fb5IggYTohM2sYhG%2BUNjZEJo%3D'
+        src={srcImage}
         alt='avatar image'
         width={width}
         height={height}
@@ -21,5 +25,3 @@ Avatar.propTypes = {
   width: PropTypes.number,
   height: PropTypes.number
 }
-
-export default Avatar
