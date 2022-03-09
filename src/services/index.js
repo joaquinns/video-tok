3550b9bfd1c635d1aa79a3bcf1a11ad9e2308572
joaquinns <joaquindevv96@gmail.com>
2022-03-09 13:35:37 -0400

    added upload and publish video services

--- src/services/index.js
+++ src/services/index.js
@@ -1,11 +1,33 @@
 import { supabase } from './supabase'
 
+const prefix = import.meta.env.VITE_SUPABASE_STORAGE_URL
+
 export const getVideos = async () => {
   const { data: videos, error } = await supabase.from('videos').select(`
-    *,
-    users (
-      username
+    *
     )
   `)
   return [error, videos]
+}
+
+export const uploadVideo = async ({ videoFile }) => {
+  const filename = window.crypto.randomUUID()
+
+  const { data, error } = await supabase.storage
+    .from('videos')
+    .upload(`uploads/${filename}.mp4`, videoFile)
+
+  const videoURL = data?.Key ? `${prefix}${data.Key}` : ''
+
+  return [error, videoURL]
+}
+
+export const publishVideo = async ({ userId, videoSrc, videoDescription }) => {
+  const { data, error } = await supabase
+    .from('videos')
+    .insert([{ user_id: userId, description: videoDescription, src: videoSrc }])
+
+  console.log(await supabase.from('videos').select(`src, user_id(email)`))
+
+  return [error, data]
 }
