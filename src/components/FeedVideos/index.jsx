eb0c90a125438909187a20f8b91732bceca08558
joaquinns <joaquindevv96@gmail.com>
2022-03-11 17:21:34 -0400

    refactor added fetchVideos

--- src/components/FeedVideos/index.jsx
+++ src/components/FeedVideos/index.jsx
@@ -1,18 +1,40 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { getVideos } from '../../services'
 import PostVideo from '../PostVideo'
 import styles from './feedVideos.module.css'
 
 export default function FeedVideos() {
+  const [postVideos, setPostVideos] = useState([])
+  const [error, setError] = useState(null)
+
+  const fetchVideos = async () => {
+    const [error, videos] = await getVideos()
+    if (error) console.error(error)
+    setError(error)
+    setPostVideos(videos)
+  }
+
+  console.log(error)
+
   useEffect(() => {
-    getVideos().then(([error, videos]) => {
-      console.log({ error, videos })
-    })
+    fetchVideos()
   }, [])
 
   return (
     <div className={styles.feed_container}>
-      <PostVideo />
+      {postVideos.length > 0 ? (
+        postVideos.map((postVideo) => (
+          <PostVideo
+            key={postVideo}
+            srcVideo={postVideo.src}
+            description={postVideo.description}
+            avatarURL={postVideo.user_id.avatar_url}
+            username={postVideo.user_id.username}
+          />
+        ))
+      ) : (
+        <h1>No videos to show x.x</h1>
+      )}
     </div>
   )
 }
