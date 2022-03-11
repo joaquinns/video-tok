b58ed3a72fd8d6f5806f98d17b447c448b4db4d9
joaquinns <joaquindevv96@gmail.com>
2022-03-11 17:18:37 -0400

    postVideo refactor

--- src/components/PostVideo/index.jsx
+++ src/components/PostVideo/index.jsx
@@ -5,7 +5,12 @@ import usePlayer from '../../hooks/usePlayer'
 import PropTypes from 'prop-types'
 import Avatar from '../Avatar'
 
-export default function PostVideo() {
+export default function PostVideo({
+  description,
+  srcVideo,
+  avatarURL,
+  username
+}) {
   const { handlePlay, playing, videoRef } = usePlayer()
 
   const playerClassname = clsx(styles.player, {
@@ -15,24 +20,25 @@ export default function PostVideo() {
   return (
     <div className={styles.video_post}>
       <div className={styles.video_post_header}>
-        <Avatar />
+        <Avatar srcImage={avatarURL} />
 
         <div className={styles.video_post_username}>
-          <h3>Username</h3>
+          <h3>{username}</h3>
           <h4>name</h4>
         </div>
       </div>
 
-      <p className={styles.video_post_description}>A simple description</p>
+      <p className={styles.video_post_description}>{description}</p>
       <div className={styles.video_wrapper}>
         <div className={styles.video}>
           <div className={styles.video_player_container}>
             <video
-              src='https://v16-webapp.tiktok.com/3f8e3800117ad1d222d1b7f61dd617ce/622429f5/video/tos/useast2a/tos-useast2a-pve-0068/4494a842ed3245ba81e84502b01599bf/?a=1988&br=2130&bt=1065&cd=0%7C0%7C1%7C0&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=XOQ9-3LPnz7Th63gCDXq&l=2022030521251201022308213817BB694C&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=anRxOTo6ZnNvOzMzNzczM0ApPDhnNjM8ODxnNzw4aDk4OmdoYmFicjRfci1gLS1kMTZzczFhMWBjLy4yX2NhYmA0MDE6Yw%3D%3D&vl=&vr='
+              src={srcVideo}
               ref={videoRef}
               controls={false}
               onClick={handlePlay}
               loop
+              preload='none'
             ></video>
             <i className={playerClassname} onClick={handlePlay}></i>
           </div>
