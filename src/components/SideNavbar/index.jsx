16fb98ad4d1f074d01952ed5929085ac75586e84
joaquinns <joaquindevv96@gmail.com>
2022-03-14 17:32:15 -0400

    active links logic

--- src/components/SideNavbar/index.jsx
+++ src/components/SideNavbar/index.jsx
@@ -1,12 +1,14 @@
 import styles from './sidenavbar.module.css'
 import { HomeIcon } from '../Icons/HomeIcon'
+import { HomeIconActive } from '../Icons/HomeIconActive'
 import { VideoIcon } from '../Icons/VideoIcon'
-import { Link } from 'wouter'
+import { Link, useRoute } from 'wouter'
 import { useAuth } from '../../context/AuthContext'
 import Avatar from '../Avatar'
 
-export default function SideNavbar() {
+export default function SideNavbar(props) {
   const { user } = useAuth()
+  const [isActive] = useRoute(props.href)
 
   return (
     <div>
@@ -14,9 +16,9 @@ export default function SideNavbar() {
         <div className={styles.scroll_styled}>
           <div className={styles.wrapper}>
             <div className={styles.main_nav_actions}>
-              <Link to='/'>
+              <Link to='/' {...props}>
                 <a className={styles.nav_link}>
-                  <HomeIcon />
+                  {isActive ? <HomeIconActive /> : <HomeIcon />}
                   <h2>Home</h2>
                 </a>
               </Link>
@@ -25,7 +27,7 @@ export default function SideNavbar() {
                 <h2>Video</h2>
               </a>
               {user && (
-                <Link to={`/profile/${user.id}`}>
+                <Link to={`/profile/${user.id}`} {...props}>
                   <a className={styles.nav_link}>
                     <Avatar
                       srcImage={user.user_metadata.avatar_url}
