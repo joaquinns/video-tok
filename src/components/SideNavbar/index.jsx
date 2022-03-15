8199d53dcffce1cdcec8904282b419422027e502
joaquinns <joaquindevv96@gmail.com>
2022-03-15 14:34:34 -0400

    added active links

--- src/components/SideNavbar/index.jsx
+++ src/components/SideNavbar/index.jsx
@@ -1,14 +1,17 @@
 import styles from './sidenavbar.module.css'
 import { HomeIcon } from '../Icons/HomeIcon'
-import { HomeIconActive } from '../Icons/HomeIconActive'
-import { VideoIcon } from '../Icons/VideoIcon'
-import { Link, useRoute } from 'wouter'
+import { Profile } from '../Icons/ProfileIcon'
 import { useAuth } from '../../context/AuthContext'
-import Avatar from '../Avatar'
+import { ActiveLink } from '../ActiveLink'
+// import Avatar from '../Avatar'
+// import clsx from 'clsx'
 
-export default function SideNavbar(props) {
+export default function SideNavbar() {
   const { user } = useAuth()
-  const [isActive] = useRoute(props.href)
+
+  /*   const activeLink = clsx(styles.nav_link, {
+    [styles.nav_link_active]: isActive
+  }) */
 
   return (
     <div>
@@ -16,27 +19,15 @@ export default function SideNavbar(props) {
         <div className={styles.scroll_styled}>
           <div className={styles.wrapper}>
             <div className={styles.main_nav_actions}>
-              <Link to='/' {...props}>
-                <a className={styles.nav_link}>
-                  {isActive ? <HomeIconActive /> : <HomeIcon />}
-                  <h2>Home</h2>
-                </a>
-              </Link>
-              <a className={styles.nav_link}>
-                <VideoIcon />
-                <h2>Video</h2>
-              </a>
+              <ActiveLink href='/'>
+                <HomeIcon />
+                <h2>Home</h2>
+              </ActiveLink>
               {user && (
-                <Link to={`/profile/${user.id}`} {...props}>
-                  <a className={styles.nav_link}>
-                    <Avatar
-                      srcImage={user.user_metadata.avatar_url}
-                      width={40}
-                      height={40}
-                    />
-                    <h2>{user.user_metadata.user_name}</h2>
-                  </a>
-                </Link>
+                <ActiveLink href={`/profile/${user.id}`}>
+                  <Profile />
+                  <h2>Profile</h2>
+                </ActiveLink>
               )}
             </div>
           </div>
