980db3606af3d2a6d4a87ce94edc667951e7636d
joaquinns <joaquindevv96@gmail.com>
2022-04-06 16:42:58 -0400

    refactor to add useGetVideos custom hook

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,36 +1,11 @@
-import { useEffect, useState } from 'react'
 import styles from './profile.module.css'
 import PostVideo from '../../components/PostVideo'
-import { getUserVideos } from '../../services/index'
 import PostVideoSkeleton from '../../components/PostVideoSkeleton'
 import Error from '../../components/Error'
+import { useGetVideos } from '../../hooks/useGetVideos'
 
 export default function Profile({ userId }) {
-  const [loading, setLoading] = useState(false)
-  const [error, setError] = useState(false)
-  const [profilePosts, setProfilePosts] = useState([])
-
-  useEffect(() => {
-    const controller = new AbortController()
-    const signal = controller.signal
-    const fetchProfileVideos = async () => {
-      setLoading(true)
-      const [error, videos] = await getUserVideos(userId, signal)
-      if (error && error.code === 20) {
-        return console.log('aborted')
-      }
-
-      if (error) setError(true)
-
-      setProfilePosts(videos)
-      setLoading(false)
-    }
-    fetchProfileVideos()
-
-    return () => {
-      controller.abort()
-    }
-  }, [])
+  const { error, loading, postVideos } = useGetVideos({ userId })
 
   return (
     <div className={styles.profile_container}>
@@ -41,13 +16,16 @@ export default function Profile({ userId }) {
         <PostVideoSkeleton />
       ) : (
         !error &&
-        profilePosts.map((postVideo) => (
+        postVideos.map((postVideo) => (
           <PostVideo
             key={postVideo.id}
             srcVideo={postVideo.src}
             description={postVideo.description}
             avatarURL={postVideo.user_id.avatar_url}
             username={postVideo.user_id.username}
+            postId={postVideo.id}
+            likes={postVideo.likes.length}
+            userPost={postVideo.user_id.id}
           />
         ))
       )}
