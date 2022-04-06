f444df5f27c7b0e07b29b5471a20d94650a3d1c8
joaquinns <joaquindevv96@gmail.com>
2022-04-06 16:38:29 -0400

    created like count custom hook

---
+++ src/hooks/useLikesCount.jsx
@@ -0,0 +1,29 @@
+import { useState, useEffect } from 'react'
+import { likeVideo, unlikeVideo, checkLike } from '../services'
+
+export const useLikesCount = ({ userId, postId, likes }) => {
+  const [likesNumber, setLikesNumber] = useState(likes)
+  const [like, setLike] = useState(false)
+
+  const toggleLike = () => {
+    setLike(!like)
+    if (like) {
+      unlikeVideo(postId, userId).then((res) =>
+        setLikesNumber((prev) => prev - 1)
+      )
+    } else {
+      likeVideo(postId, userId).then((res) =>
+        setLikesNumber((prev) => prev + 1)
+      )
+    }
+  }
+
+  useEffect(() => {
+    checkLike(postId, userId).then((data) => {
+      const liked = data[1][0]?.likes?.find((likeUser) => likeUser === userId)
+      liked ? setLike(true) : setLike(false)
+    })
+  }, [postId, userId])
+
+  return { toggleLike, likesNumber, like }
+}
