09df27b1778a57b6374a9c57785cd369ae044545
joaquinns <joaquindevv96@gmail.com>
2022-04-06 16:42:35 -0400

    refactor to add useGetVideos custom hook

--- src/components/FeedVideos/index.jsx
+++ src/components/FeedVideos/index.jsx
@@ -1,40 +1,19 @@
-import { useEffect, useState } from 'react'
-import { getVideos } from '../../services'
 import PostVideo from '../PostVideo'
+import Error from '../Error'
 import PostVideoSkeleton from '../PostVideoSkeleton'
 import styles from './feedVideos.module.css'
+import { useGetVideos } from '../../hooks/useGetVideos'
 
 export default function FeedVideos() {
-  const [loading, setLoading] = useState(false)
-  const [postVideos, setPostVideos] = useState([])
-  // const [error, setError] = useState(null)
-
-  useEffect(() => {
-    const controller = new AbortController()
-    const signal = controller.signal
-    const fetchVideos = async () => {
-      setLoading(true)
-      const [error, videos] = await getVideos(signal)
-      if (error && error.code === 20) {
-        return console.log('aborted')
-      } else {
-        console.log(error)
-      }
-      setPostVideos(videos)
-      setLoading(false)
-    }
-    fetchVideos()
-
-    return function cleanup() {
-      controller.abort()
-    }
-  }, [])
+  const { error, loading, postVideos } = useGetVideos({})
 
   return (
     <div className={styles.feed_container}>
+      {error && <Error />}
       {loading ? (
         <PostVideoSkeleton />
       ) : (
+        !error &&
         postVideos.map((postVideo) => (
           <PostVideo
             key={postVideo.id}
@@ -42,6 +21,9 @@ export default function FeedVideos() {
             description={postVideo.description}
             avatarURL={postVideo.user_id.avatar_url}
             username={postVideo.user_id.username}
+            postId={postVideo.id}
+            likes={postVideo.likes.length}
+            userPost={postVideo.user_id.id}
           />
         ))
       )}
