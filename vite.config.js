2c137ffbcfae771ded7ae8f481958aa4aef8868f
joaquinns <joaquindevv96@gmail.com>
2022-04-07 00:11:04 -0400

    adding config

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  esbuild: {
-    jsxFactory: '_jsx',
-    jsxFragment: '_jsxFragment',
-    jsxInject: 'import React from "react"'
-  },
   plugins: [react()]
 })
