c89c233fc9924f39269e06e77ccf4727abfb557b
joaquinns <joaquindevv96@gmail.com>
2022-04-06 22:03:39 -0400

    refactor

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -3,19 +3,32 @@ import PostVideo from '../../components/PostVideo'
 import PostVideoSkeleton from '../../components/PostVideoSkeleton'
 import Error from '../../components/Error'
 import { useGetVideos } from '../../hooks/useGetVideos'
+import { useLocation } from 'wouter'
 
 export default function Profile({ userId }) {
+  const [, setLocation] = useLocation()
   const { error, loading, postVideos } = useGetVideos({ userId })
+
+  const handleCreate = () => {
+    setLocation('/upload')
+  }
+
+  if (error) {
+    return (
+      <div className={styles.profile_container}>
+        <h1>Profile</h1>
+        <Error />
+      </div>
+    )
+  }
 
   return (
     <div className={styles.profile_container}>
-      {error && <Error />}
       <h1>Profile</h1>
 
       {loading ? (
         <PostVideoSkeleton />
       ) : (
-        !error &&
         postVideos.map((postVideo) => (
           <PostVideo
             key={postVideo.id}
@@ -24,10 +37,22 @@ export default function Profile({ userId }) {
             avatarURL={postVideo.user_id.avatar_url}
             username={postVideo.user_id.username}
             postId={postVideo.id}
-            likes={postVideo.likes.length}
+            likes={!postVideo.likes ? 0 : postVideo.likes?.length}
             userPost={postVideo.user_id.id}
           />
         ))
+      )}
+
+      {postVideos.length < 1 && (
+        <>
+          <h1>You don&apos;t have any post!</h1>
+          <button
+            className={styles.button_redirect_create}
+            onClick={handleCreate}
+          >
+            go!
+          </button>
+        </>
       )}
     </div>
   )
