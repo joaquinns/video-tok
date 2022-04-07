d7df29b3c46f5b22ac5d446bf00438ad888c8a8a
joaquinns <joaquindevv96@gmail.com>
2022-04-06 22:16:17 -0400

    deleting example src

--- src/components/Avatar/index.jsx
+++ src/components/Avatar/index.jsx
@@ -1,11 +1,7 @@
 import PropTypes from 'prop-types'
 import styles from './avatar.module.css'
 
-export default function Avatar({
-  srcImage = 'https://p16-sign-va.tiktokcdn.com/tos-maliva-avt-0068/12762135a3b5593013a8e94aabc4015f~c5_100x100.jpeg?x-expires=1646604000&x-signature=ZtQ%2Fb5IggYTohM2sYhG%2BUNjZEJo%3D',
-  width = 60,
-  height = 60
-}) {
+export default function Avatar({ srcImage, width = 60, height = 60 }) {
   return (
     <div className={styles.avatar_container}>
       <img
