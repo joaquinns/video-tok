dad1b92165092a6479cb8da7304a5dd8a8061fac
joaquinns <joaquindevv96@gmail.com>
2022-04-07 13:47:51 -0400

    path vite config

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  publicDir: 'public',
-  build: {
+  publicDir: 'public'
+  /*   build: {
     rollupOptions: {
       output: {
         chunkFileNames: 'assets/js/[name]-[hash].js',
@@ -26,5 +26,5 @@ export default defineConfig({
         }
       }
     }
-  }
+  } */
 })
